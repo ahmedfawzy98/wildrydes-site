7e8301b0ed30842b8bc8da75d0b087dfebb1105c
ahmed <ahmedfawzy671998@gmailcom>
2021-04-14 13:08:04 +0200

    Update config file with cognito information

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_BmJm2Dcmx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '9k7hc3i32hcbllf5q5ro1k711', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
